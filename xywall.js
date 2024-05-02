07d0847e7f03216589d9426a408b9f6aaf1a8c1a
Yuzihang9 <yuzihang789@gmail.com>
2024-05-02 19:15:34 +0800

    xywall

---
+++ xywall.js
@@ -0,0 +1,12 @@
+[rewrite_local]
+https://sdk.xiangyubizhi.com/(v2/front/photo/getOne|v2/front/member/info) url script-response-body xywall.js
+***********************/
+    [mitm]
+hostname = sdk.xiangyubizhi.com
+    ************************************* /
+var body = $response.body
+    .replace(/"vip":\s*\d/g, '"vip":1')
+    .replace(/"vipName":\s*\w+/g, `"vipName":"”¿æ√ª·‘±"`)
+    .replace(/"endTime":\s*\w+/g, '"endTime":"2099-12-12"')
+    .replace(/false/g, "true");
+$done({ body });
