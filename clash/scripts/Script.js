49844b561accace5813c7d3cad11ef6dbd30a046
Lucas <86992887+Lucasss1916@users.noreply.github.com>
2024-10-05 07:56:48 +0800

    Update Script.js

--- clash/scripts/Script.js
+++ clash/scripts/Script.js
@@ -277,7 +277,8 @@ function main(config) {
         },
     });
 
-    config["rules"] = [
+    config["rules"] = [                     
+        "IP-CIDR,6.6.6.6/32,DIRECT,no-resolve"
         "RULE-SET,microsoft,Microsoft",  
         "RULE-SET,github,Github",   
         "RULE-SET,spotify,Spotify",
